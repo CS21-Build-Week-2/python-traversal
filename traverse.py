ae15cfe2db407c4a40bd1d0d56ad333b35c99900
Alfonso <alfonso.garcia2324@gmail.com>
2019-10-23 12:42:19 -0700

    able to make request

--- traverse.py
+++ traverse.py
@@ -3,6 +3,7 @@ from flip_dir import flip_dir
 
 import requests
 import time
+import os
 
 endpoints = {
     'init': "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/",
@@ -10,3 +11,16 @@ endpoints = {
     'take': "https://lambda-treasure-hunt.herokuapp.com/api/adv/take/",
     'drop': 'https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/'
 }
+
+
+# Goal is to visit all 500 rooms
+# We need to store room information somewhere. With python, we can use the open() function
+# use the stack to back-track in the graph. when we move in a direction, push the opposite direction to the stack
+# keep track of the cooldown as well, use time.sleep() for that
+
+r = requests.get(url=endpoints['init'], headers={
+                 "Authorization": "Token 5859b714ecceab7b49085ecf1222f2adc318b86e"})
+
+data = r.json()
+
+# print(data)
